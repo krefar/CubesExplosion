033702c64d143da36cf367a83bb2555d5012a65f
Nbotvin <nbotvin@korusconsulting.ru>
2024-05-12 19:12:25 +0300

    Rework by 2.0 task

--- Assets/Scripts/Services/Subdevision/Subdividing.cs
+++ Assets/Scripts/Services/Subdevision/Subdividing.cs
@@ -12,7 +12,7 @@ public class Subdividing : MonoBehaviour
     [SerializeField] private Explosion _explosion;
 
     private float _divideChance = 1.0f;
-    private float _explosionEnd = 0.16f;
+    private float _explosionEnd = 0.12f;
 
     public void Init(float divideChance = 1.0f)
     {
@@ -24,35 +24,24 @@ public class Subdividing : MonoBehaviour
         if (_divideChance >= Random.value)
         {
             var resultCount = Random.Range(_minCount, _maxCount + 1);
-            var objects = CreateObjects(resultCount);
-            
-            HideGameObject();
-
-            var explosion = CreateExplosion();
-
-            StartCoroutine(ExplosionEnd(objects, explosion));
+            CreateObjects(resultCount);
         }
         else
         {
-            Destroy(gameObject);
+            var explosion = CreateExplosion();
+
+            StartCoroutine(ExplosionEnd(explosion));
         }
+
+        HideGameObject();
     }
 
-    private IEnumerator ExplosionEnd(List<GameObject> objects, Explosion explosion)
+    private IEnumerator ExplosionEnd(Explosion explosion)
     {
         yield return new WaitForSeconds(_explosionEnd);
 
-        MoveToCubesLayer(objects);
         Destroy(explosion.gameObject);
         Destroy(gameObject);
-    }
-
-    private void  MoveToCubesLayer(List<GameObject> objects)
-    {
-        foreach (GameObject obj in objects)
-        {
-            obj.layer = LayerMask.NameToLayer(Layers.Cubes.ToString());
-        }
     }
 
     private Explosion CreateExplosion()
@@ -64,21 +53,14 @@ public class Subdividing : MonoBehaviour
         return explosion;
     }
 
-    private List<GameObject> CreateObjects(int count)
+    private void CreateObjects(int count)
     {
-        var result = new List<GameObject>();
-
         while (count-- > 0)
         {
             var newObject = Instantiate(gameObject);
 
             EnsureProps(newObject);
-
-            result.Add(newObject);
-
         }
-
-        return result;
     }
 
     private void HideGameObject()
@@ -93,7 +75,6 @@ public class Subdividing : MonoBehaviour
     private void EnsureProps(GameObject newObject)
     {
         newObject.transform.localScale = transform.localScale / 2;
-        newObject.layer = LayerMask.NameToLayer(Layers.Explosion.ToString());
 
         var subdivided = newObject.GetComponent<Subdividing>();
         subdivided.Init(_divideChance / 2);
